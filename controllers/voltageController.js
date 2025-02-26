b2356311be7b394e86536973bfff022d28b65cbf
Matthew Zhigalo <matthewzhigalo@email.com>
2025-02-26 16:58:42 +0300

    Changed sql statement to select data more then reference

--- controllers/voltageController.js
+++ controllers/voltageController.js
@@ -1,16 +1,23 @@
-import { json } from 'express';
 import pool from '../config/db.js';
 import moment from 'moment';
 
 const formatter = 'YYYY-MM-DD HH:mm:ss';
+const averageJSON ={
+    v_magnetic: 0.0922,
+    v_piezo: 0.52456,
+    v_12: 0.5300,
+    v_24: 4.5330
+};
 
 const getVoltages = (req, res) => {
     const data = req.query.data;
     const startDate = moment.unix(Date.parse(req.query.startDate)/1000).format(formatter);
     const endDate = moment.unix(Date.parse(req.query.endDate)/1000).format(formatter);
-
-    pool.query(`SELECT * FROM voltage_logs WHERE hwtime BETWEEN '${startDate}' AND '${endDate}'`, (err, results) => {
+    const sqlStatement = `SELECT hwtime, ${data} FROM voltage_logs WHERE ${data} > ${parseFloat(averageJSON[data])} GROUP BY hwtime, ${data} HAVING hwtime BETWEEN '${startDate}' AND '${endDate}' ORDER BY hwtime`;
+    
+    pool.query(sqlStatement, (err, results) => {
         if (err) throw err;
+        console.log("Statement:\n" + sqlStatement);
         res.status(200).json(results.rows);
     });
 };
